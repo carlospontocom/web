a469f59af668e27db35b2dc201f63b2b7bbbadba
frontendcnn <frontendcnn@gmail.com>
2026-05-14 02:54:04 +0000

    Menu desktop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import AppHader from './components/AppHeader.vue'
 </script>
 
 <template>
+  <AppHader/>
   <HelloWorld />
 </template>
+
+<style lang="scss">
+@import "./styles/style.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header>
+        <nav class="navbar">
+
+        <h3>VueTS</h3>
+            <ul class="menu">
+                <li>
+                    <a href="">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Login
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Cadastro
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+
+<style scoped lang="scss">
+@import '../styles/appHeader.scss';
+</style>
